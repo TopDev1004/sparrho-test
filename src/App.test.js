0e1d5d9af7ee7f2086f7ad57f9f2aefea8e5f1f8
TopDev <top.dev.1004@gmail.com>
2020-07-16 05:59:39 -0700

    add unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,18 @@
+import { convertNumToWord } from "./utils/converter";
 
+describe("Num Convert Test", () => {
+  it("should return correct string of the 4 numbers", () => {
+    expect(convertNumToWord(9874)).toBe(
+      "Nine Thousand Eight Hundred And Seventy Four"
+    );
+  });
+  it("should return correct string of the 3 numbers", () => {
+    expect(convertNumToWord(374)).toBe("Three Hundred And Seventy Four");
+  });
+  it("should return correct string of the 2 number", () => {
+    expect(convertNumToWord(54)).toBe("Fifty Four");
+  });
+  it("should return correct string of a number", () => {
+    expect(convertNumToWord(3)).toBe("Three");
+  });
+});
